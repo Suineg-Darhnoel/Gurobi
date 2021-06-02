3fe5d022e2ae687335aca4d2ca729a229d4a7ebb
CHANRATHNAK BORANN <borannchanrathnak@gmail.com>
2021-06-02 19:42:57 +0900

    Solve Kakkuro

--- Kakkuro/kakkuro.py
+++ Kakkuro/kakkuro.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
-# THIS PROGRAM AIMS TO SOLVE KAKKURO
-# BY INTEGER PROGRAMMING USING GUROBIPY
+"""
+This program aims to solve kakkuro
+using IP with the help of gurobipy library
+"""
 
-import gurobipy as gp
-import numpy as np
+import gurobipy as gp # library for integer programming
+import numpy as np # for convinience
 from gurobipy import GRB
-
 
 # UTIL FUNCTIONS
 # decode coordinate to value
@@ -35,7 +36,7 @@ def test():
 # Blank(1) and Occupied(0) cells
 b = np.array([
         [0, 1, 1, 0, 0],
-        [0, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1],
         [1, 1, 0, 1, 1],
         [1, 1, 1, 1, 1],
         [0, 0, 1, 1, 0]
@@ -85,9 +86,6 @@ pattern_num = 9 # [1-9]
 # -- Create Model
 kmodel = gp.Model("kakkuro")
 
-# Set no objective function
-kmodel.setObjective(0)
-
 # -- Add Variables
 # Create variables for every blank cell
 cell_vars = kmodel.addVars(
@@ -96,65 +94,91 @@ cell_vars = kmodel.addVars(
             pattern_num,
             vtype=GRB.BINARY,
             name='X'
-        )
-
-# Add constraints for unused variables (Occupied cell)
-occupied_vars_constr = kmodel.addConstrs(
-            (cell_vars[i, j, k] == 0
-                for i in range(row_num)
-                for j in range(col_num)
-                for k in range(pattern_num)
-                if b[i, j] == 0
-            ),
-            name="OV"
-        )
+        ) # [checked]
 
 # CONSTRAINTS
-
-# Add Horizontal Constraints
-# -- No redundant numbers in the consecutive cells
-h_constr = kmodel.addConstrs(
+# Cell Constraints
+# -- One of the values [1-9] must be taken
+cell_constr = kmodel.addConstrs(
             (cell_vars.sum(x, y+i, '*') == 1
                 for x in range(row_num)
                 for y in range(col_num)
-                for i in range(coord2range(hhints, (x, y)))),
-            name="H"
+                for i in range(coord2range(hhints, (x, y)))
+                if b[x, y+i] == 1),
+            name="CELL"
+        ) # [checked]
+
+# Add Horizontal Constraints
+# -- No redundant values in the same horizontal block
+h_constr = kmodel.addConstrs(
+            (cell_vars[i, j1, v]+cell_vars[i, j2, v] <= 1
+                for i in range(row_num)
+                for j1 in range(col_num)
+                for j2 in range(col_num)
+                for v in range(pattern_num)
+                if b[i, j1] == 1 and b[i, j2] == 1 and [i, j1] != [i, j2])
         )
 
-# -- Each sum of the consecutive cells is equal to the given value
+# -- Each sum of the consecutive hcells is equal to the given value
 hsum_constr = kmodel.addConstrs(
-            (gp.quicksum((v+1)*cell_vars.sum(x, y+i, v)
-                for v in range(pattern_num)) == coord2val(hhints, (x, y))
+            (gp.quicksum((v+1)*cell_vars[x, y+i, v]
+                for v in range(pattern_num)
+                for i in range(coord2range(hhints, (x, y)))
+                if b[x, y+i] == 1) == coord2val(hhints, (x, y))
+
                 for x in range(row_num)
                 for y in range(col_num)
-                for i in range(coord2val(hhints, (x, y)))
-                # if b[x, y] == 1
+                if coord2val(hhints, (x, y)) != 0
             ),
             name="H_SUM"
-        )
+        ) # [checked]
 
 # Add Vertical Constraints
+# -- No redundant values in the same vertical block
 v_constr = kmodel.addConstrs(
-            (cell_vars.sum(x+i, y, '*') == 1
-                for x in range(row_num)
-                for y in range(col_num)
-                for i in range(coord2range(vhints, (x, y)))),
-            name="V"
+            (cell_vars[i1, j, v]+cell_vars[i2, j, v] <= 1
+                for i1 in range(row_num)
+                for i2 in range(row_num)
+                for j in range(col_num)
+                for v in range(pattern_num)
+                if b[i1, j] == 1 and b[i2, j] == 1 and [i1, j] != [i2, j])
         )
-
+# -- Each sum of the consecutive vcells is equal to the given value
 vsum_constr = kmodel.addConstrs(
-            (gp.quicksum((v+1)*cell_vars.sum(x, y+i, v)
-                for v in range(pattern_num)) == coord2val(vhints, (x, y))
+            (gp.quicksum((v+1)*cell_vars[x+i, y, v]
+                for v in range(pattern_num)
+                for i in range(coord2range(vhints, (x, y)))
+                if b[x+i, y] == 1) == coord2val(vhints, (x, y))
+
                 for x in range(row_num)
                 for y in range(col_num)
-                for i in range(coord2val(vhints, (x, y)))
-                # if b[x, y] == 1
+                if coord2val(vhints, (x, y)) != 0
             ),
             name="V_SUM"
-        )
+        ) # [checked]
+
+# Set no objective function
+kmodel.setObjective(0)
 
 # Optimize
 kmodel.optimize()
+
+# Print Solutions
+import re
+# print(kmodel.printAttr('x'))
+
+for v in kmodel.getVars():
+    value = v.getAttr(GRB.Attr.X)
+    name  = v.getAttr(GRB.Attr.VarName)
+    row, col, ans = re.findall('[0-9]+', name)
+    if value == 1:
+        print("({}, {}) = {}".format(row, col, int(ans)+1))
+
+# Write a solution to a file
+try:
+    kmodel.write("kakkuro.sol")
+except gp.GurobiError as e:
+    print("Error: ", e)
 
 # Write model to a file
 kmodel.write('kakkuro.lp')
